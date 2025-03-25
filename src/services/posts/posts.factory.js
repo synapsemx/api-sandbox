395e78fb138d2c5b598491a528c66fb97bbc2309
Joseph Chambers <joseph@michael-chambers.com>
2025-03-25 03:00:37 -0400

    refactor: improve post factories

--- src/services/posts/posts.factory.js
+++ src/services/posts/posts.factory.js
@@ -1,5 +1,8 @@
 import { faker } from '@faker-js/faker'
 import { insert } from '../../utils/factory.js'
+import { createCategory } from '../categories/categories.factory.js'
+import { createTagPost } from '../tag-post/tag-post.factory.js'
+import { createTag } from '../tags/tags.factory.js'
 import { postsTable } from './posts.shared.js'
 
 export function generatePost(attributes = {}) {
@@ -13,4 +16,36 @@ export function generatePost(attributes = {}) {
 
 export function createPost(attributes = {}) {
   return insert(generatePost(attributes), postsTable)
+}
+
+export async function createPostWithCategory({
+  attributes = {},
+  categoryAttributes = {}
+} = {}) {
+  const category = await createCategory(categoryAttributes)
+  const post = await createPost({ category_id: category.id, ...attributes })
+
+  return { category, post }
+}
+
+export async function createPostWithTags({
+  attributes = {},
+  categoryAttributes = {},
+  tagsAttributes = []
+} = {}) {
+  const { post, category } = await createPostWithCategory({
+    attributes,
+    categoryAttributes
+  })
+  const tags = []
+
+  for (const tagAttributes of tagsAttributes) {
+    const tag = await createTag(tagAttributes)
+
+    await createTagPost({ post_id: post.id, tag_id: tag.id })
+
+    tags.push(tag)
+  }
+
+  return { post, category, tags }
 }
--- test/lib/knex/join-relation.support.test.js
+++ test/lib/knex/join-relation.support.test.js
@@ -1,40 +1,23 @@
 import { describe, expect, it } from 'vitest'
 import { app } from '../../../src/app.js'
 import { createCategory } from '../../../src/services/categories/categories.factory.js'
-import { createPost } from '../../../src/services/posts/posts.factory.js'
-import { createTagPost } from '../../../src/services/tag-post/tag-post.factory.js'
-import { createTag } from '../../../src/services/tags/tags.factory.js'
+import {
+  createPostWithCategory,
+  createPostWithTags
+} from '../../../src/services/posts/posts.factory.js'
 import { resetDatabaseUsingTransaction } from '../../helpers/db.js'
 
 describe('Knex Adapter Extended - Join Relationship Support', () => {
   resetDatabaseUsingTransaction()
-
-  const createPostWithCategory = async (categoryName = null) => {
-    const category = await createCategory(
-      categoryName ? { name: categoryName } : {}
-    )
-    const post = await createPost({ category_id: category.id })
-
-    return { category, post }
-  }
-
-  const createPostWithTags = async (tagNames = []) => {
-    const { post } = await createPostWithCategory()
-    const tags = await Promise.all(tagNames.map((name) => createTag({ name })))
-
-    await Promise.all(
-      tags.map((tag) => createTagPost({ post_id: post.id, tag_id: tag.id }))
-    )
-
-    return { post, tags }
-  }
 
   it('performs a join on a belongsTo relationship (post → category)', async () => {
     const targetCategoryName = 'Target Category'
 
     await createPostWithCategory()
     await createPostWithCategory()
-    const { category } = await createPostWithCategory(targetCategoryName)
+    const { category } = await createPostWithCategory({
+      categoryAttributes: { name: targetCategoryName }
+    })
 
     const posts = await app.service('posts').find({
       query: {
@@ -48,9 +31,9 @@ describe('Knex Adapter Extended - Join Relationship Support', () => {
   })
 
   it('performs a join on a hasMany relationship (category → posts)', async () => {
-    const { category, post } = await createPostWithCategory('With Post')
+    const { category, post } = await createPostWithCategory()
 
-    await createCategory({ name: 'Without Post' })
+    await createCategory()
 
     const categories = await app.service('categories').find({
       query: {
@@ -64,9 +47,11 @@ describe('Knex Adapter Extended - Join Relationship Support', () => {
   })
 
   it('performs a join on a manyToMany relationship (post ↔ tags)', async () => {
-    await createPostWithTags(['Common'])
-    await createPostWithTags(['Other'])
-    const { post } = await createPostWithTags(['Target'])
+    await createPostWithTags({ tagsAttributes: [{ name: 'Common' }] })
+    await createPostWithTags({ tagsAttributes: [{ name: 'Other' }] })
+    const { post } = await createPostWithTags({
+      tagsAttributes: [{ name: 'Target' }]
+    })
 
     const posts = await app.service('posts').find({
       query: {

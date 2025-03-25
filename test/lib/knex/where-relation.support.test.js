60c4ca07240526024b46417bb4ac9a40b5005bb0
Joseph Chambers <joseph@michael-chambers.com>
2025-03-25 03:18:17 -0400

    test: improve where relation tests

--- test/lib/knex/where-relation.support.test.js
+++ test/lib/knex/where-relation.support.test.js
@@ -72,4 +72,100 @@ describe('Knex Adapter Extended - Where Relationship Support', () => {
     expect(posts.data.length).toBe(1)
     expect(posts.data[0].id).toBe(post.id)
   })
+
+  it('does not return results when $whereRelation (belongsTo) condition does not match', async () => {
+    await createPostWithCategory({
+      categoryAttributes: { name: 'Mismatch' }
+    })
+
+    const posts = await app.service('posts').find({
+      query: {
+        $whereRelation: {
+          category: {
+            'categories.name': 'Nonexistent'
+          }
+        }
+      }
+    })
+
+    expect(posts.data.length).toBe(0)
+  })
+
+  it('does not return results when $whereRelation (hasMany) condition does not match', async () => {
+    await createPostWithCategory()
+    await createCategory()
+
+    const categories = await app.service('categories').find({
+      query: {
+        $whereRelation: {
+          posts: {
+            'posts.content': 'Nonexistent'
+          }
+        }
+      }
+    })
+
+    expect(categories.data.length).toBe(0)
+  })
+
+  it('does not return results when $whereRelation (manyToMany) condition does not match', async () => {
+    await createPostWithTags({
+      tagsAttributes: [{ name: 'Irrelevant' }]
+    })
+
+    const posts = await app.service('posts').find({
+      query: {
+        $whereRelation: {
+          tags: {
+            'tags.name': 'Nonexistent'
+          }
+        }
+      }
+    })
+
+    expect(posts.data.length).toBe(0)
+  })
+
+  it('supports multiple $whereRelation conditions combined with $or', async () => {
+    await createPostWithTags({ tagsAttributes: [{ name: 'Alpha' }] })
+    await createPostWithTags({ tagsAttributes: [{ name: 'Beta' }] })
+    const { post } = await createPostWithTags({
+      tagsAttributes: [{ name: 'Gamma' }]
+    })
+
+    const posts = await app.service('posts').find({
+      query: {
+        $whereRelation: {
+          tags: {
+            $or: [{ 'tags.name': 'Gamma' }, { 'tags.name': 'Delta' }]
+          }
+        }
+      }
+    })
+
+    expect(posts.data.length).toBe(1)
+    expect(posts.data[0].id).toBe(post.id)
+  })
+
+  it('supports $whereRelation with multiple conditions on a single related record', async () => {
+    const { post, tags } = await createPostWithTags({
+      tagsAttributes: [{ name: 'TagX' }]
+    })
+
+    const [tag] = tags
+
+    const posts = await app.service('posts').find({
+      query: {
+        $whereRelation: {
+          tags: {
+            'tags.name': tag.name,
+            'tags.id': tag.id
+          }
+        }
+      }
+    })
+
+    expect(posts.data.length).toBe(1)
+    expect(posts.data[0].id).toBe(post.id)
+  })
 })

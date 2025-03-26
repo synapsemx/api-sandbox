443988b6ec465393cfc2ee81e88858f7c3cae2c3
Joseph Chambers <joseph@michael-chambers.com>
2025-03-26 06:58:24 -0400

    test: finish many-to-many tests

--- test/hooks/include-relationships/many-to-many.handler.test.js
+++ test/hooks/include-relationships/many-to-many.handler.test.js
@@ -1,5 +1,8 @@
 import { describe, it } from 'vitest'
-import { createPostWithTags } from '../../../src/services/composite-factories.js'
+import {
+  createPostWithRelatedPosts,
+  createPostWithTags
+} from '../../../src/services/composite-factories.js'
 import { resetDatabaseUsingTransaction } from '../../helpers/db.js'
 import { instantiateHttp, setupServer } from '../../helpers/http.js'
 
@@ -27,6 +30,33 @@ describe('Include Relationships hook - Many to many handler', () => {
 
     for (const tag of tags) {
       expect(response.relations.tags.some((t) => t.id === tag.id)).toBe(true)
+    }
+  })
+
+  it('should include self-referencing many-to-many relationship (post ↔ post)', async () => {
+    const http = instantiateHttp()
+
+    const { post, relatedPosts } = await createPostWithRelatedPosts({
+      relatedPostsAttributes: [{}, {}, {}]
+    })
+
+    const { data: response } = await http.get('/posts?$include=related_posts')
+    const postFromApi = response.data.find((p) => p.id === post.id)
+
+    expect(postFromApi).toBeDefined()
+    expect(Array.isArray(postFromApi.related_posts)).toBe(true)
+    expect(postFromApi.related_posts.sort()).toEqual(
+      relatedPosts.map((p) => p.id).sort()
+    )
+
+    expect(response.relations).toBeDefined()
+    expect(Array.isArray(response.relations.posts)).toBe(true)
+    expect(response.relations.posts).toHaveLength(3)
+
+    for (const relatedPost of relatedPosts) {
+      expect(
+        response.relations.posts.some((p) => p.id === relatedPost.id)
+      ).toBe(true)
     }
   })
 })
